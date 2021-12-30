057379b50363baf64c9f0ec7db4bf60d2d62c5db
Stephen Kurshner <skurshner@gmail.com>
2021-12-30 12:01:00 -0800

    added hero buttons

---
+++ script.js
@@ -0,0 +1,15 @@
+const openMenu = () => {
+  document.getElementById("email-dropdown").classList.toggle("show");
+};
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
